b04feb769d97f992a4f54079e3dc49c66081e64a
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-25 16:10:19 -0400

    Icon App

---
+++ IconApp/IconApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IconApp
+//
+//  Created by Alfonso Almonte Espinosa on 25/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IconApp/IconApp/IconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconAppApp.swift
+//  IconApp
+//
+//  Created by Alfonso Almonte Espinosa on 25/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
